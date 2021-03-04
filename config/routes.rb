e2fa4de38ca7892e8f2c6e6901eb39bda66ce483
Sabia Tahir <sabiatahir@hotmail.com>
2021-03-04 20:43:51 +0000

    Add login route and display user name on login

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,10 @@ Rails.application.routes.draw do
   end
 
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   get "/signup", to: 'users#new'
   post "/users", to: 'users#create'
 

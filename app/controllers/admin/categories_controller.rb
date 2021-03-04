e94e1fd7d261b095d3e675f1c936065147ab2d72
Sabia Tahir <sabiatahir@hotmail.com>
2021-03-04 17:17:04 +0000

    Add form to create new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,12 +3,21 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.order(id: :desc).all
-    
+
+    @quantity = Product.where(category_id: 3).sum(:quantity)
   end
 
   def new 
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(params.require(:category).permit(:name))
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 end

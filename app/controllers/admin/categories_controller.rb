c6394c30c75a6a3f66b83bfb22e22f9dc5c4a90f
Sabia Tahir <sabiatahir@hotmail.com>
2021-03-04 17:23:34 +0000

    Clean up categories path

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,8 +3,6 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.order(id: :desc).all
-
-    @quantity = Product.where(category_id: 3).sum(:quantity)
   end
 
   def new 
@@ -13,7 +11,6 @@ class Admin::CategoriesController < ApplicationController
 
   def create
     @category = Category.new(params.require(:category).permit(:name))
-
     if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
